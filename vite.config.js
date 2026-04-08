8e5c5ecc8b751364f0b9e050149f43a936b80899
SeuNome <seu-email@gmail.com>
2026-04-08 19:27:07 -0300

    Remove plugin copy-redirects e arquivos conflitantes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { copyFileSync } from 'fs'; // Importe a função de cópia
 export default defineConfig({
-    plugins: [
-        react(),
-        {
-            name: 'copy-redirects',
-            closeBundle() {
-                // Este código roda ao final do processo de build
-                copyFileSync('public/_redirects', 'dist/_redirects');
-                console.log('✅ Arquivo _redirects copiado para a pasta dist!');
-            },
-        },
-    ],
+    plugins: [react()],
 });
